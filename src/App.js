5e26b279367395b1264df1965ce017db4ed3acc9
AdekWHATT <vyacheslaw.kilin@mail.ru>
2021-09-27 22:26:14 +0300

    Adding Files 27.09.2021

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Layout from "./hoc/Layout/Layout";
+import React from "react";
+import Quiz from "./containers/Quiz/Quiz";
+function App() {
+  return(
+ <Layout>
+ <Quiz/>
+ </Layout>
+  )
+}
+
+export default App;
