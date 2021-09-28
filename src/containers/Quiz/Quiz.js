46ae24477a37e6ab9d2e8c18655f195a5e4a0999
AdekWHATT <vyacheslaw.kilin@mail.ru>
2021-09-28 12:19:27 +0300

    до 6.9

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -5,6 +5,7 @@ import ActiveQuiz from "../../components/ActiveQuiz/ActiveQuiz";
 class Quiz extends Component {
     state = {
         activeQuestion: 0,
+        answerState: null, // { [id]: 'succes' 'error'}
         quiz: [
             {
                 question: 'Какого цвета небо?',
@@ -37,10 +38,35 @@ class Quiz extends Component {
         ]
     }
     onAnswerClickHandler = (answerId) => {
-        console.log('Answer ID', answerId)
-        this.setState({
-            activeQuestion: this.state.activeQuestion + 1
-        })
+        // console.log('Answer ID', answerId)
+        const question = this.state.quiz[this.state.activeQuestion]
+        if (question.rightAnswerId === answerId) {
+            this.setState({
+                answerState: {[answerId]: 'succes'}
+            })
+                const timeout = window.setTimeout(() => {
+                    if (this.isQuizFinished()) {
+                            console.log('Finished')
+                    } else {
+                        this.setState({
+                            activeQuestion: this.state.activeQuestion + 1,
+                            answerState: null
+                        })
+                    }
+
+                    window.clearTimeout(timeout)
+                },1000)
+
+            
+        } else {
+            this.setState({
+                answerState: {[answerId]: 'error'}
+            })
+        }
+       
+    }
+    isQuizFinished() {
+        return this.state.activeQuestion + 1 === this.state.quiz.length
     }
     render() {
         return (
@@ -55,6 +81,7 @@ class Quiz extends Component {
                         onAnswerClick={this.onAnswerClickHandler}
                         quizLength={this.state.quiz.length}
                         answerNumber={this.state.activeQuestion + 1}
+                        state={this.state.answerState}
                     />
 
                 </div>
