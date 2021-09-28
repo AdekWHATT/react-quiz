086618be4179d7ecbb0a6b65ca051d356ca60dc4
AdekWHATT <vyacheslaw.kilin@mail.ru>
2021-09-28 16:31:06 +0300

    13. Компонент кнопки и ее стилизация

--- src/components/FinishedQuiz/FinishedQuiz.js
+++ src/components/FinishedQuiz/FinishedQuiz.js
@@ -1,5 +1,6 @@
 import React from "react";
 import classes from './FinishedQuiz.module.css'
+import Button from "../UI/Button/Button";
 const FinishedQuiz = props => {
     const successCount = Object.keys(props.results).reduce((total, key) => {
         if (props.results[key] === 'success') {
@@ -28,7 +29,8 @@ const FinishedQuiz = props => {
                 </ul>
                 <p>Правильно {successCount} из {props.quiz.length}</p>
                 <div>
-                    <button onClick={props.onRetry}>Повторить</button>
+                    <Button onClick={props.onRetry} type={'primary'}>Повторить</Button>
+                    <Button type={'success'}>Перейти в список тестов</Button>
                 </div>
         </div> 
     )
