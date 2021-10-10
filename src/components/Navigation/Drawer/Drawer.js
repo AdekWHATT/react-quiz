88b449355f132b51d73ea67a6d2537de33d53ba9
AdekWHATT <vyacheslaw.kilin@mail.ru>
2021-10-10 14:33:15 +0300

    навигация компонента Drawer(выпадающего меню)

--- src/components/Navigation/Drawer/Drawer.js
+++ src/components/Navigation/Drawer/Drawer.js
@@ -1,14 +1,42 @@
 import React, {Component} from "react";
 import classes from './Drawer.module.css'
 import Backdrop from "../../UI/Backdrop/Backdrop";
-const links = [1,2,3]
+import { NavLink } from "react-router-dom";
+// каждый элемент массива links представляется в виде объекта
+const links = [ // и у каждого обьекта есть свои параметры
+    {to: '/', // говорим куда будет произведена навигация в данной ссылке
+    label: 'Список', // название данной ссылки
+    exact: true}, // тут true и она будет считываться несмотря на префикс после /
+    
+    // поэтому мы указываем напрямую что нужно только точное совпадение
+
+    {to: '/auth', 
+    label: 'Авторизация', 
+    exact: false}, // false т.к. у них будут отдельные страницы и они ни скем не будут пересекаться
+
+    {to: '/quiz-creator', 
+    label: 'Создать тест', 
+    exact: false}
+]
+
 
 class Drawer extends Component {
-    renderLinks () {
+    clickHandler = () => {
+        this.props.onClose()
+    }
+    renderLinks () { // пробегается по массиву links и генерирует теги li и NavLink
         return links.map((link,index) => {
             return (
                 <li key={index}>
-                  <a>Ссылка{link}</a>
+                <NavLink
+                to={link.to}
+                exact={link.exact}
+                activeClassName={classes.active}
+                onClick={this.clickHandler} // закрывает выпадающий список после выбора
+                >
+                    {/* {link.label} - выведет название ссылок */}
+                    {link.label} 
+                </NavLink>
                 </li>
             )
         })
