b6e8a8583c1edfe88522959e99774bde1dae334a
Вячеслав Килин <vyacheslaw.kilin@mail.ru>
2021-12-29 23:11:16 +0300

    Перепишу ка я компонент quizlist с классовой на функциональную

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,8 +1,9 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import classes from './QuizList.module.css'
 import { NavLink } from "react-router-dom";
 import axios from "../../axios/axios-quiz";
 import Loader from "../../components/UI/Loader/Loader";
+
 export default class QuizList extends Component {
 
     state = {
@@ -11,54 +12,54 @@ export default class QuizList extends Component {
     }
 
     renderQuizes() {
-        return this.state.quizes.map((quiz) => { // возвращаем массиы из 3 элементов,впоследствии данный массив заменим на то что приходит с сервера
-           // на казжой итерации получим quiz и index
+        return this.state.quiz.map((quiz) => { // возвращаем массиы из 3 элементов,впоследствии данный массив заменим на то что приходит с сервера
+            // на казжой итерации получим quiz и index
             return ( // вернуть должны jsx и корневой элемент списка должен быть <li>, т.к.  {this.renderQuizes()} находится с теге <ul>
                 <li
-                key={quiz.id}
-                > 
-                {/* контент который будет представлять тег li это будет ссылка NavLink */}
+                    key={quiz.id}
+                >
+                    {/* контент который будет представлять тег li это будет ссылка NavLink */}
                     <NavLink to={'/quiz/' + quiz.id}>
-                      {/* впоследствии в навлинк будет динамический id, но пока как-то так)) */}
-                       {quiz.name} 
+                        {/* впоследствии в навлинк будет динамический id, но пока как-то так)) */}
+                        {quiz.name}
 
                     </NavLink>
                 </li>
             )
         })
     }
-// Компонент жизненного цикла в котором идет обращение к серверу
+    // Компонент жизненного цикла в котором идет обращение к серверу
 
-async componentDidMount() {
-    try {
- const responce = await axios.get('/quizes.json')
-    const quizes = []   
-    Object.keys(responce.data).forEach((key, index) => {
-        quizes.push({
-            id: key,
-            name: `Тест №${index + 1 }`
-        })
-        })
-        this.setState({
-            quizes, loading: false
-        })
-} catch (e) {
-    console.log(e)
-}
-}
+    async componentDidMount() {
+        try {
+            const responce = await axios.get('/quizes.json')
+            const quizes = []
+            Object.keys(responce.data).forEach((key, index) => {
+                quizes.push({
+                    id: key,
+                    name: `Тест №${index + 1}`
+                })
+            })
+            this.setState({
+                quizes, loading: false
+            })
+        } catch (e) {
+            console.log(e)
+        }
+    }
 
     render() {
-        return(
+        return (
             <div className={classes.QuizList}>
                 <div>
-                <h1>Список тестов:</h1>
-                {
-            
-            this.state.loading
-            ? <Loader/>
-            : <ul> {this.renderQuizes()} </ul>
-            }
-               
+                    <h1>Список тестов:</h1>
+                    {
+
+                        this.state.loading
+                            ? <Loader />
+                            : <ul> {this.renderQuizes()} </ul>
+                    }
+
                 </div>
             </div>
         )
